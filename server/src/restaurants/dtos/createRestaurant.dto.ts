ace99c9e4645c8e5a88e2a902fdbca20a8094aa3
evantianx <evantianx@gmail.com>
2021-01-02 09:25:52 +0800

    feat: validate entities props with class-validator
    Signed-off-by: evantianx <evantianx@gmail.com>

--- server/src/restaurants/dtos/createRestaurant.dto.ts
+++ server/src/restaurants/dtos/createRestaurant.dto.ts
@@ -1,8 +1,10 @@
 import { ArgsType, Field } from '@nestjs/graphql';
+import { Length } from 'class-validator';
 
 @ArgsType()
 export class CreateRestaurantDto {
   @Field(() => String)
+  @Length(3, 8)
   name: string;
 
   @Field(() => Boolean)
