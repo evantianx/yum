6992e4adf7b004296403d6ef82576852f5ccba93
evantianx <evantianx@gmail.com>
2021-01-03 09:29:59 +0800

    feat: typeorm and crossenv configuration
    Signed-off-by: evantianx <evantianx@gmail.com>

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
+    TypeOrmModule.forRoot(),
     RestaurantsModule,
   ],
   controllers: [],
