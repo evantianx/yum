0a3802cbee0842e5237bfb352ddc1e2b264774c1
evantianx <evantianx@gmail.com>
2021-01-04 09:54:46 +0800

    feat: inject service into resolvers and repository into service
    Signed-off-by: evantianx <evantianx@gmail.com>

--- server/src/restaurants/restaurants.resolver.ts
+++ server/src/restaurants/restaurants.resolver.ts
@@ -1,12 +1,15 @@
 import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 import { CreateRestaurantDto } from './dtos/createRestaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
+import { RestaurantService } from './restautant.service';
 
 @Resolver(() => Restaurant)
 export class RestaurantsResolver {
+  constructor(private readonly restaurantService: RestaurantService) {}
+
   @Query(() => [Restaurant])
-  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
-    return [];
+  restaurants(): Promise<Restaurant[]> {
+    return this.restaurantService.getAll();
   }
 
   @Mutation(() => Boolean)
